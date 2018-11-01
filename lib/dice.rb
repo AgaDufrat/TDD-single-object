8645440d9c98ec7fda0bbf7b443976caf17061de
Aga <a.dufrat@gmail.com>
2018-11-01 10:42:21 +0000

    Rolls any number of dice at the same time

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,11 @@
 class Dice
+  def initialize(num = 1)
+    @num_of_dice = num
+  end
+
   def roll
-    rand(1..6)
+    sum = 0
+    @num_of_dice.times { sum += rand(1..6) }
+    sum
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -9,6 +9,10 @@ describe '#roll' do
   it 'returns number between 1 - 6' do
     expect(subject.roll).to be_between(1, 6).inclusive
   end
+
+  it 'rolls any number of dice at the same time' do
+    expect(Dice.new(2).roll).to be_between(2, 12).inclusive
+  end
 end
 
 end
