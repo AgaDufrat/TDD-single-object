afccdb5f5a517598c6021ea8084a2b9a0c90911a
Aga <a.dufrat@gmail.com>
2018-11-01 09:49:48 +0000

    Roll returns number between 1 - 6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -5,4 +5,10 @@ describe Dice do
     expect(subject).to respond_to(:roll)
   end
 
+describe '#roll' do
+  it 'returns number between 1 - 6' do
+    expect(subject.roll).to eq(1)
+  end
+end
+
 end
